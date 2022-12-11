f28706fdc6c5db82467f6e528fc6c44fbd9a0611
Igor Lobach <lobach.igor.olegovich@gmail.com>
2022-12-11 20:11:49 +0200

    Changed file name hw_4_functions.py, added descriptions to functions, checked with pylint

--- hw_4_functions.py
+++ hw_4_functions.py
@@ -1,5 +1,10 @@
+"""
+Homework for lesson 4. Functions and pretty code
+"""
+
+
 def largest_of_two_numbers(num1: float = 0, num2: float = 0) -> float:
-    # compare two numbers and return the largest.
+    """Compare two numbers and return the largest."""
     if num1 > num2:
         result = num1
     else:
@@ -8,7 +13,7 @@ def largest_of_two_numbers(num1: float = 0, num2: float = 0) -> float:
 
 
 def smallest_of_three_numbers(num1: float = 0, num2: float = 0, num3: float = 0) -> float:
-    # compare three numbers and return the smallest.
+    """Compare three numbers and return the smallest."""
     if num1 < num2 and num1 < num3:
         result = num1
     elif num2 < num1 and num2 < num3:
@@ -19,38 +24,40 @@ def smallest_of_three_numbers(num1: float = 0, num2: float = 0, num3: float = 0)
 
 
 def abs_of_number(num: float = 0) -> float:
-    # return the absolute value of the number.
+    """Return the absolute value of the number."""
     if num < 0:
         num = - num
     return num
 
 
 def sum_of_two_numbers(num1: float = 0, num2: float = 0) -> float:
-    # print the sum of two numbers.
+    """Print the sum of two numbers."""
     print(f"The sum of 1st and 2nd numbers is {num1 + num2}")
-    return
 
 
 def number_sign(num: float = 0):
-    # print the sign of the number.
+    """Print the sign of the number."""
     if num < 0:
         print(f"Number {num} is negative")
     elif num > 0:
         print(f"Number {num} is positive")
     else:
         print("It's zero")
-    return
 
 
+# check functions
 number_first = float(input("Enter 1st number: ", ))
 number_second = float(input("Enter 2nd number: ", ))
 number_third = float(input("Enter 3rd number: ", ))
 print("-" * 20 + "FUNCTION 1" + "-"*20)
-print(f"The largest of 1st and 2nd numbers is {largest_of_two_numbers(number_first, number_second)}")
+print(f"The largest of 1st and 2nd numbers is "
+      f"{largest_of_two_numbers(number_first, number_second)}")
 print("-" * 20 + "FUNCTION 2" + "-"*20)
-print(f"The smallest of three numbers is {smallest_of_three_numbers(number_first, number_second, number_third)}")
+print(f"The smallest of three numbers is "
+      f"{smallest_of_three_numbers(number_first, number_second, number_third)}")
 print("-" * 20 + "FUNCTION 3" + "-"*20)
-print(f"The absolute value of first number is {abs_of_number(number_first)}")
+print(f"The absolute value of first number is "
+      f"{abs_of_number(number_first)}")
 print("-" * 20 + "FUNCTION 4" + "-"*20)
 sum_of_two_numbers(number_first, number_second)
 print("-" * 20 + "FUNCTION 5" + "-"*20)
