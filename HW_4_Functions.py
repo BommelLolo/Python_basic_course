d6e3486cfa632d8fc60abe7851b52d8d6fa881e7
Igor Lobach <lobach.igor.olegovich@gmail.com>
2022-12-11 19:50:11 +0200

    Add file HW_4_Functions.py

---
+++ HW_4_Functions.py
@@ -0,0 +1,57 @@
+def largest_of_two_numbers(num1: float = 0, num2: float = 0) -> float:
+    # compare two numbers and return the largest.
+    if num1 > num2:
+        result = num1
+    else:
+        result = num2
+    return float(result)
+
+
+def smallest_of_three_numbers(num1: float = 0, num2: float = 0, num3: float = 0) -> float:
+    # compare three numbers and return the smallest.
+    if num1 < num2 and num1 < num3:
+        result = num1
+    elif num2 < num1 and num2 < num3:
+        result = num2
+    else:
+        result = num3
+    return result
+
+
+def abs_of_number(num: float = 0) -> float:
+    # return the absolute value of the number.
+    if num < 0:
+        num = - num
+    return num
+
+
+def sum_of_two_numbers(num1: float = 0, num2: float = 0) -> float:
+    # print the sum of two numbers.
+    print(f"The sum of 1st and 2nd numbers is {num1 + num2}")
+    return
+
+
+def number_sign(num: float = 0):
+    # print the sign of the number.
+    if num < 0:
+        print(f"Number {num} is negative")
+    elif num > 0:
+        print(f"Number {num} is positive")
+    else:
+        print("It's zero")
+    return
+
+
+number_first = float(input("Enter 1st number: ", ))
+number_second = float(input("Enter 2nd number: ", ))
+number_third = float(input("Enter 3rd number: ", ))
+print("-" * 20 + "FUNCTION 1" + "-"*20)
+print(f"The largest of 1st and 2nd numbers is {largest_of_two_numbers(number_first, number_second)}")
+print("-" * 20 + "FUNCTION 2" + "-"*20)
+print(f"The smallest of three numbers is {smallest_of_three_numbers(number_first, number_second, number_third)}")
+print("-" * 20 + "FUNCTION 3" + "-"*20)
+print(f"The absolute value of first number is {abs_of_number(number_first)}")
+print("-" * 20 + "FUNCTION 4" + "-"*20)
+sum_of_two_numbers(number_first, number_second)
+print("-" * 20 + "FUNCTION 5" + "-"*20)
+number_sign(number_first)
