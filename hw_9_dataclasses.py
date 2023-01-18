12fa3f06b07e8ff5328d0563178f33e02f0348cd
Igor Lobach <lobach.igor.olegovich@gmail.com>
2023-01-18 14:43:29 +0200

    worked out the remarks to hw

--- hw_9_dataclasses.py
+++ hw_9_dataclasses.py
@@ -7,18 +7,24 @@ class Date:
     month: int
     day: int
 
+    # second solution
     def __gt__(self, other):
         assert isinstance(other, Date)
-        res = False
-        if self.year > other.year:
-            res = True
-        elif self.year == other.year:
-            if self.month > other.month:
-                res = True
-            elif self.month == other.month:
-                if self.day > other.day:
-                    res = True
-        return res
+        return (self.year, self.month, self.day) > (other.year, other.month, other.day)
+
+    # first solution
+    # def __gt__(self, other):
+    #     assert isinstance(other, Date)
+    #     res = False
+    #     if self.year > other.year:
+    #         res = True
+    #     elif self.year == other.year:
+    #         if self.month > other.month:
+    #             res = True
+    #         elif self.month == other.month:
+    #             if self.day > other.day:
+    #                 res = True
+    #     return res
 
 
 @dataclass
@@ -26,32 +32,50 @@ class DateRange:
     date_begin: Date
     date_end: Date
 
+    def __gt__(self, other):
+        assert isinstance(other, DateRange)
+        return self.date_begin > other.date_begin
 
+
+# second solution
 def get_ranges_wo_insurance(insurance_periods: list[DateRange]) -> list[DateRange]:
     res_list = []
-    # check length of periods. If less two, than we can't define periods w/o insurance
-    if len(insurance_periods) > 1:
+    # Sort fo   r list of all insurance's periods for begin date from earlier to later
+    insurance_periods.sort()
 
-        # sort list of all insurance's periods for begin date from earlier to later
-        # for i in range(len(insurance_periods)):
-        for i, _ in enumerate(insurance_periods):
-            min_date = i
-            for j in range(i+1, len(insurance_periods)):
-                if insurance_periods[j].date_begin < insurance_periods[min_date].date_begin:
-                    min_date = j
-
-            insurance_periods[min_date], insurance_periods[i] = \
-                insurance_periods[i], insurance_periods[min_date]
-
-        # check periods w/o insurance
-        for k in range(len(insurance_periods)-1):
-            if insurance_periods[k].date_end < insurance_periods[k+1].date_begin:
-                period = DateRange(insurance_periods[k].date_end, insurance_periods[k+1].date_begin)
-                res_list.append(period)
-
-    else:
-        res_list = []
+    # check periods w/o insurance
+    for k in range(len(insurance_periods)-1):
+        if insurance_periods[k].date_end < insurance_periods[k+1].date_begin:
+            period = DateRange(insurance_periods[k].date_end, insurance_periods[k+1].date_begin)
+            res_list.append(period)
     return res_list
+
+
+# first solution
+# def get_ranges_wo_insurance(insurance_periods: list[DateRange]) -> list[DateRange]:
+#     res_list = []
+#     # check length of periods. If less two, than we can't define periods w/o insurance
+#     if len(insurance_periods) > 1:
+#
+#         # Selection Sort for list of all insurance's periods for begin date from earlier to later
+#         for i, _ in enumerate(insurance_periods):
+#             min_date = i
+#             for j in range(i+1, len(insurance_periods)):
+#                 if insurance_periods[j].date_begin < insurance_periods[min_date].date_begin:
+#                     min_date = j
+#
+#             insurance_periods[min_date], insurance_periods[i] = \
+#                 insurance_periods[i], insurance_periods[min_date]
+#
+#         # check periods w/o insurance
+#         for k in range(len(insurance_periods)-1):
+#             if insurance_periods[k].date_end < insurance_periods[k+1].date_begin:
+#                 period = DateRange(insurance_periods[k].date_end, insurance_periods[k+1].date_begin)
+#                 res_list.append(period)
+#
+#     else:
+#         res_list = []
+#     return res_list
 
 
 if __name__ == '__main__':
@@ -62,10 +86,7 @@ if __name__ == '__main__':
         DateRange(Date(2020, 10, 1), Date(2020, 12, 31)),
     ]
     get_ranges_wo_insurance(_insurances)
-    """
-    у перевірці помилка у місяці
-    було DateRange(Date(2020, 7, 31) замість DateRange(Date(2020, 8, 31)
-    """
+
     assert get_ranges_wo_insurance(_insurances) == [
         DateRange(Date(2020, 6, 25), Date(2020, 6, 29)),
         DateRange(Date(2020, 8, 31), Date(2020, 10, 1)),
