722d674a8ad15b222f40843a7ff90f2ca230fb2c
Igor Lobach <lobach.igor.olegovich@gmail.com>
2023-01-25 20:10:02 +0200

    Corrected remerks df

--- hw_11_code_with_arguments.py
+++ hw_11_code_with_arguments.py
@@ -21,34 +21,33 @@ class MathFunction:
         self.arg_2 = arg_2
         self.result = None
 
+    @property
     def add(self):
         self.result = self.arg_1 + self.arg_2
         return self.result
 
+    @property
     def subtract(self):
         self.result = self.arg_1 - self.arg_2
         return self.result
 
+    @property
     def divide(self):
         self.result = self.arg_1 / self.arg_2
         return self.result
 
+    @property
     def multiply(self):
         self.result = self.arg_1 * self.arg_2
         return self.result
-
-
-class InvalidDMethodError(Exception):
-    def __init__(self):
-        logger.error('Got invalid function name: %s', str(method))
-        sys.exit(2)
 
 
 if __name__ == '__main__':
     # check function name
     function = {'add', 'subtract', 'multiply', 'divide'}
     if method not in function:
-        raise InvalidDMethodError()
+        logger.error('Got invalid function name: %s', str(method))
+        sys.exit(2)
 
     logger.info('Function "%s" called with: %s', method, sys.argv[1:])
     #  check quantity of arguments
@@ -66,18 +65,10 @@ if __name__ == '__main__':
         sys.exit(2)
 
     c = MathFunction(a_int, b_int)
-    # use function for object
-    if method == 'add':
-        c.add()
-    elif method == 'subtract':
-        c.subtract()
-    elif method == 'multiply':
-        c.multiply()
-    elif method == 'divide':
-        try:
-            c.divide()
-        except ZeroDivisionError:
-            logger.exception('Tried to divide by 0')
-            sys.exit(2)
+    try:
+        getattr(c, method)
+    except ZeroDivisionError:
+        logger.exception('Tried to divide by 0')
+        sys.exit(2)
     # log result
     logger.info('Result: %s', c.result)
